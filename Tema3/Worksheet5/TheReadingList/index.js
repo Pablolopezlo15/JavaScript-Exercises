b57f69a157b37764dac3a7955e55ac03c7f96e5a
Pablo Lopez Lozano <plopezlozano12@gmail.com>
2023-10-17 10:19:04 +0200

    Todo menos el DATE

--- Tema3/Worksheet5/TheReadingList/index.js
+++ Tema3/Worksheet5/TheReadingList/index.js
@@ -1,36 +1,74 @@
 class BookList{
-    constructor(NumReaded,NumNotReaded,NextBook,CurrentBook,LastBook,AllBooks) {
+    constructor() {
 
-        this.NumReaded = NumReaded;
-        this.NumNotReaded = NumNotReaded;
-        this.NextBook = NextBook;
-        this.CurrentBook = 0;
+        this.NumReaded = 0;
+        this.NumNotReaded = 0;
+        this.CurrentBook = -1;
         this.LastBook = -1;
         this.AllBooks = [];
-
     }
+
     add(Book){
-        this.AllBooks.push(book);
-        if (this.currentBookIndex === 0) {
-            this.currentBookIndex = this.findFirstUnreadBookIndex();
+        if(this.AllBooks.length == 0){
+            this.AllBooks.push(Book);
+            this.CurrentBook++;
+            this.NumNotReaded++;
         }
-        new Book(Title,Genre,Author,false,null);
-    }
-    finishCurrentBook(){
+        else{
+            this.AllBooks.push(Book);
+            this.NumNotReaded++;
 
+        }
     }
+
+    currentBook(){
+        if(this.CurrentBook == -1) {
+            console.log("No tienes libros para leer");
+        }
+        else {
+            console.log("El libro que estás leyendo es: "+this.AllBooks[this.CurrentBook].Title)
+        }
+    }
+
+    finishCurrentBook(){
+        console.log("Has terminado un libro")
+        this.AllBooks[this.CurrentBook].Read = true;
+        this.CurrentBook++;
+        this.NumReaded++;
+        this.ReadDate = Date();
+    }
+
+    nextBook(){
+        if(this.CurrentBook == 0){
+            let next = this.CurrentBook+1;
+
+        }
+    }
+    
 }
 
 class Book{
-    constructor(Title,Genre,Author,Read,ReadDate){
+    constructor(Title,Genre,Author){
         this.Title = Title;
         this.Genre = Genre;
         this.Author = Author;
-        this.Read = Read;
-        this.ReadDate = ReadDate;
+        this.Read = false;
+        this.ReadDate = null;
     }
 }
 
-add("Don Quijote", "Aventura", "Miguel de Cervantes");
-console.log();
 
+Lista1 = new BookList();
+Libro1 = new Book("Don Quijote", "Aventura", "Miguel de Cervantes",false);
+Libro2 = new Book("El Señor de los Anillos", "Ficción", "J.R.R Tolkien", false);
+Libro3 = new Book("Libro1", "Ficción", "J.R.R Tolkien", false);
+Libro4 = new Book("Libro2", "Ficción", "J.R.R Tolkien", false);
+Libro5 = new Book("Libro3", "Ficción", "J.R.R Tolkien", false);
+Lista1.add(Libro1);
+Lista1.add(Libro2);
+Lista1.add(Libro3);
+
+Lista1.currentBook();
+Lista1.finishCurrentBook();
+Lista1.currentBook();
+console.log(Lista1.AllBooks);
