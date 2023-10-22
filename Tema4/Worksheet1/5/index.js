b4206a525ecd8d027e545deff35469ef0397b8dd
Pablolopezlo15 <plopezlozano12@gmail.com>
2023-10-23 00:01:09 +0200

    update

--- Tema4/Worksheet1/5/index.js
+++ Tema4/Worksheet1/5/index.js
@@ -1,18 +1,25 @@
 function asociarEventos() {
     const celdas = document.querySelectorAll("table td");
+    celdas.forEach(celda => {
+        celda.addEventListener("mousemove", pintar);
+        celda.addEventListener("mousedown", borrarLinea);
+    });
 
-    celdas.forEach(celda => {celda.addEventListener("mousemove", pintar)});
+    const borrarCanvasButton = document.getElementById("borrarCanvas");
+    borrarCanvasButton.addEventListener("click", borrarCanvas);
 }
 
-
-function borrar(e) {
-    if(e.ctrlKey) {
+function borrarLinea(e) {
+    if (e.ctrlKey) {
         e.target.style.backgroundColor = "red";
-    }
-    else if(e.shiftKey) {
+    } else if (e.shiftKey) {
         e.target.style.backgroundColor = "blue";
-
     }
-
 }
 
+function borrarCanvas() {
+    const celdas = document.querySelectorAll("table td");
+    celdas.forEach(celda => {
+        celda.style.backgroundColor = "white";
+    });
+}
