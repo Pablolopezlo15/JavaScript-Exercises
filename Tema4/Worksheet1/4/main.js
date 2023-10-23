96b97467c814112c1ef1a12d1ebc0c02c5c0ba6a
Pablo Lopez Lozano <plopezlozano12@gmail.com>
2023-10-23 08:31:57 +0200

    update

--- Tema4/Worksheet1/4/main.js
+++ Tema4/Worksheet1/4/main.js
@@ -2,9 +2,9 @@ window.onload = () =>{
     console.log("CARGADA PAGINA");
     
     document.write('<table border="1" >');
-    for (i = 0; i<100; i++){
+    for (i = 0; i<70; i++){
         document.write("<tr></tr>")
-        for(j = 0; j<100; j++){
+        for(j = 0; j<200; j++){
             document.write("<td></td>")
         }
     }
