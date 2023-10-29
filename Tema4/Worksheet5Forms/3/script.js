0c5319a0d5149790e68b14d88f159643bee345bf
Pablolopezlo15 <plopezlozano12@gmail.com>
2023-10-29 11:46:27 +0100

    Ej 3

--- Tema4/Worksheet5Forms/3/script.js
+++ Tema4/Worksheet5Forms/3/script.js
@@ -1,28 +1,20 @@
-
-function obtenerValor() {
-    const valor = username.value;
-    console.log("Texto actual en el input:", valor);
-}
-const username = document.getElementById("username");
-const valor = username.value;
-
-username.addEventListener("input", obtenerValor);
+const usernameInput = document.getElementById("username");
 
 function validarMayuscula(valor) {
     const errormayus = document.getElementById("errormayus");
-    if (/[A-Z]/.test(valor)){
+    if (/[A-Z]/.test(valor)) {
         errormayus.style.color = "green";
-    }
+    } 
     else {
-        errorminus.style.color = "red";
+        errormayus.style.color = "red";
     }
 }
 
 function validarMinúscula(valor) {
     const errorminus = document.getElementById("errorminus");
-    if (/[a-z]/.test(valor)){
+    if (/[a-z]/.test(valor)) {
         errorminus.style.color = "green";
-    }
+    } 
     else {
         errorminus.style.color = "red";
     }
@@ -30,16 +22,19 @@ function validarMinúscula(valor) {
 
 function validarLongitud(valor) {
     const errorlong = document.getElementById("errorlong");
-    if (valor.lenght >= 6) {
+    if (valor.length >= 6) {
         errorlong.style.color = "green";
-
+    } 
+    else {
+        errorlong.style.color = "red";
     }
 }
 
-const inputusername = document.getElementById("username");
-inputusername.addEventListener("keydown", validarLongitud);
-inputusername.addEventListener("keydown", validarMayuscula);
-inputusername.addEventListener("keydown", validarMinúscula);
+function validar() {
+    const valor = usernameInput.value;
+    validarMayuscula(valor);
+    validarMinúscula(valor);
+    validarLongitud(valor);
+}
 
-
-
+usernameInput.addEventListener("keyup", validar);
