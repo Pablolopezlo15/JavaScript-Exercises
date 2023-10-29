ab36f12d7ecceb7d5e03d0a73f19ab49e3a9fec5
Pablolopezlo15 <plopezlozano12@gmail.com>
2023-10-29 22:27:28 +0100

    Update

--- Tema4/Worksheet5Forms/5/script.js
+++ Tema4/Worksheet5Forms/5/script.js
@@ -65,5 +65,26 @@ inputcardholders.addEventListener("input", parteDelante);
 inputcvv.addEventListener("input", escribirValores);
 inputcvv.addEventListener("input", vueltaCvv);
 
+function validarTarjetaNum() {
+    const regEx = /^(?:4\d([\- ])?\d{6}\1\d{5}|(?:4\d{3}|5[1-5]\d{2}|6011)([\- ])?\d{4}\2\d{4}\2\d{4})$/;
 
+}
 
+function validarCvv(valorcvv) {
+
+    const cvvRegEx = /^[0-9]{3}$/;
+    const errorcvv = document.getElementById("errorcvv");
+    if (cvvRegEx.test(valorcvv)) {
+        console.log('número de seguridad válido');
+        return true;
+    }
+    else {
+        console.log('número de seguridad inválido');
+        errorcvv.innerHTML = "CVV no válido";
+        return false;
+    }
+}
+
+validarCvv();
+
+inputcvv.addEventListener("input", validarCvv);
