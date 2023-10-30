cd289ca7ba68ad736bab3d36286bf4af13bb9069
Pablolopezlo15 <plopezlozano12@gmail.com>
2023-10-30 21:14:16 +0100

    Update index.js

--- Tema4/Worksheet6/4/index.js
+++ Tema4/Worksheet6/4/index.js
@@ -1,6 +1,5 @@
 window.onload = ()=> {
-    let fecha = Date.now();
-    CrearCookie("colorFondo","red", fecha.toUTCString);
+    CrearCookie("colorFondo","red", Date());
     LeerCookie("colorFondo");
 
 }
@@ -20,4 +19,15 @@ function LeerCookie(identificador) {
 
 function CrearCookie(identificador,valor,fechaExpiracion){
     document.cookie = identificador+"="+valor+"; expires="+fechaExpiracion;
+}
+
+function BorrarCookie(identificador) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        const cookieParts = cookie.split('=');
+        if (cookieParts[0] == identificador) {
+            
+        }
+    }
 }
