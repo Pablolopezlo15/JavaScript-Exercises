4dc1fade0c91585d0c41fe63e896c0eea3401fb0
Pablolopezlo15 <plopezlozano12@gmail.com>
2023-11-01 20:05:59 +0100

    update

--- Tema4/Worksheet6/1/index.js
+++ Tema4/Worksheet6/1/index.js
@@ -1,43 +1,41 @@
 window.onload = () => {
+    nombre = document.getElementById("nombre").addEventListener("click", setName);
+    borrarcookie = document.getElementById("borrarcookie").addEventListener("click", deleteCookie);
 
 }
-function setCookie() {
-    var userName = document.getElementById("nameInput").value;
-    document.cookie = "userName=" + userName + "; expires=" + getCookieExpiration(5);
-    showGreeting(userName);
+
+function setName() {
+    var name = prompt("Por favor, ingresa tu nombre:");
+    if (name) {
+        var d = new Date();
+        d.setTime(d.getTime() + (5 * 60 * 1000));
+        document.cookie = "user_name=" + encodeURIComponent(name) + "; expires=" + d.toUTCString() + "; path=/";
+        displayGreeting(name);
+    }
 }
 
-// Función para obtener la fecha de expiración de la cookie en minutos
-function getCookieExpiration(minutes) {
-    var now = new Date();
-    now.setTime(now.getTime() + (minutes * 60 * 1000));
-    return now.toUTCString();
+function mostrarNombre(name) {
+    var mostrarnombre = document.getElementById("mostrarnombre");
+    mostrarnombre.innerHTML = "Hola, " + name + ". Bienvenido a nuestra página.";
 }
 
-// Función para mostrar el saludo al usuario
-function showGreeting(userName) {
-    document.getElementById("content").style.display = "none";
-    document.getElementById("message").style.display = "block";
-    document.getElementById("userName").textContent = userName;
-}
-
-// Función para borrar la cookie y cerrar la sesión
 function deleteCookie() {
-    document.cookie = "userName=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-    document.getElementById("content").style.display = "block";
-    document.getElementById("message").style.display = "none";
-    document.getElementById("nameInput").value = "";
+    document.cookie = "user_name=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+    var mostrarnombre = document.getElementById("mostrarnombre");
+    mostrarnombre.innerHTML = "";
 }
 
-// Comprobar si la cookie ya existe
+// Comprobar si existe una cookie al cargar la página
 function checkCookie() {
     var cookies = document.cookie.split(';');
     for (var i = 0; i < cookies.length; i++) {
         var cookie = cookies[i].trim();
-        if (cookie.indexOf("userName=") === 0) {
-            var userName = cookie.substring(9);
-            showGreeting(userName);
-            return;
+        if (cookie.indexOf("user_name=") === 0) {
+            var name = decodeURIComponent(cookie.substring(10));
+            mostrarNombre(name);
+            break;
         }
     }
 }
+
+checkCookie();
