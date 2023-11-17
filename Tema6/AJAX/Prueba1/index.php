66750bae8fbed8073ec4ff507b114446b291a492
Pablo Lopez Lozano <plopezlozano12@gmail.com>
2023-11-17 12:53:14 +0100

    AJAX

---
+++ Tema6/AJAX/Prueba1/index.php
@@ -0,0 +1,18 @@
+<?php
+    $pais = "España";
+    $habitantes = 47000000;
+    $capital = "Madrid";
+    $moneda = "Euro";
+    $idioma = "Español";
+    $bandera = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/1200px-Flag_of_Spain.svg.png";
+
+    echo "País: " . $pais . "<br>";
+    echo "Habitantes: " . $habitantes . "<br>";
+    echo "Capital: " . $capital . "<br>";
+    echo "Moneda: " . $moneda . "<br>";
+    echo "Idioma: " . $idioma . "<br>";
+    echo "Bandera: " . $bandera . "<br>";
+    
+
+
+?>
